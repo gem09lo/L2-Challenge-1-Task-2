ff21aed9d459f447e86c6e1b043be83c2477c6f8
Your Name <gemmaw09lo@gmail.com>
2024-07-31 19:53:12 +0100

    Add to Github

---
+++ L2_Challenge1_Task2.py
@@ -0,0 +1,16 @@
+##L2 Challenge 1: Task 2 - python L2_Challenge1_Task2.py
+##Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+def digitize(n):
+    n = str(n)
+    #lst = []
+    #for d in n:
+        #lst.append(int(d))
+    #return lst[::-1]
+    lst = [int(d) for d in n]
+    return lst[::-1]
+
+
+print (digitize(35231))
+print (digitize(0))
+print (digitize(23582357))
